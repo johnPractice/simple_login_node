055cf0c305a4086126748ba2f0ded9784d2989f9
Reza Mousavian <rezamosavian1014@gmail.com>
2020-10-07 10:59:43 -0700

    add npm

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const express = req
+const express = require("express");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.clear();
+    console.log(`server runnig at port ${port}`);
+});
