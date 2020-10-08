07e331fa1bc82f4bdb943c7a26548954ed14d311
Reza Mousavian <rezamosavian1014@gmail.com>
2020-10-08 02:42:49 -0700

    add user tokens

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,21 +1,25 @@
 const rout = require("express").Router();
 
-const User = require('../db/model/user');
+const User = require("../db/model/user");
 
 rout.get("/user", (req, res) => {
     res.json("Hi user");
 });
 
-rout.post('/user', async(req, res) => {
+rout.post("/user", async(req, res) => {
     const info = req.body;
-    console.log('info', info);
+    console.log("info", info);
+
     try {
         const user = new User(info);
         await user.save();
-        res.json(user);
+        const token = await user.genrateAuth();
+        res.json({
+            user,
+            token
+        });
     } catch (e) {
         res.json(e).status(400);
     }
-
 });
 module.exports = rout;
